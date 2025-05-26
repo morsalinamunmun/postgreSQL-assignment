e800dc2dd6af5acf0edbc6d351df01725b1db0c6
morsalinamunmun <morsalinamunmun@gmail.com>
2025-05-26 20:03:13 +0600

    Comment out SELECT statements for rangers and species tables; add ORDER BY clause to ranger sightings query

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -11,7 +11,7 @@ INSERT INTO rangers (ranger_id, name, region) VALUES
 (2, 'Bob White', 'River Delta'),
 (3, 'Carol King', 'Mountain Range');
 
-SELECT * FROM rangers;
+-- SELECT * FROM rangers;
 
 -- species table
 CREATE TABLE species (
@@ -22,7 +22,7 @@ CREATE TABLE species (
     conservation_status VARCHAR(50) NOT NULL
 );
 
-SELECT * FROM species;
+-- SELECT * FROM species;
 
 -- Insert sample data into species
 INSERT INTO species (species_id, common_name, scientific_name, discovery_date, conservation_status) VALUES
@@ -48,7 +48,7 @@ INSERT INTO sightings (sighting_id, species_id, ranger_id, location, sighting_ti
 (3, 3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
 (4, 1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
 
-SELECT * FROM sightings;
+-- SELECT * FROM sightings;
 
 -- Problem 1: Register a new ranger
 SELECT setval('rangers_ranger_id_seq', (SELECT MAX(ranger_id) FROM rangers));
@@ -66,7 +66,8 @@ WHERE location LIKE '%Pass%';
 -- Problem 4: List each ranger's name and their total number of sightings
 SELECT r.name, COUNT(s.sighting_id) AS total_sightings
 FROM rangers r JOIN sightings s ON r.ranger_id = s.ranger_id
-GROUP BY r.name;
+GROUP BY r.name
+ORDER BY r.name;
 
 -- Problem 5: List species that have never been sighted
 SELECT common_name FROM species
