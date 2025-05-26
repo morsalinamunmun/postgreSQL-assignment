76244bc81a16d00ef5b1e021d6e3c63aff808986
morsalinamunmun <morsalinamunmun@gmail.com>
2025-05-26 12:57:32 +0600

    Refactor table comments and add sightings table with sample data

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,4 +1,4 @@
--- Problem-1: rangers table
+-- rangers table
 CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -13,7 +13,7 @@ INSERT INTO rangers (ranger_id, name, region) VALUES
 
 SELECT * FROM rangers;
 
--- Problem-2: species table
+-- species table
 CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
     common_name VARCHAR(100) NOT NULL,
@@ -30,3 +30,31 @@ INSERT INTO species (species_id, common_name, scientific_name, discovery_date, c
 (2, 'Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
 (3, 'Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
 (4, 'Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+-- sightings table
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INT REFERENCES species(species_id),
+    ranger_id INT REFERENCES rangers(ranger_id),
+    location VARCHAR(150) NOT NULL,
+    sighting_time TIMESTAMP NOT NULL,
+    notes TEXT
+);
+
+-- Insert sample data into sightings
+INSERT INTO sightings (sighting_id, species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(4, 1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+SELECT * FROM sightings;
+
+-- Problem 1: Register a new ranger
+SELECT setval('rangers_ranger_id_seq', (SELECT MAX(ranger_id) FROM rangers));
+
+INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains');
+
+-- Problem 2: Count unique species ever sighted
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
+FROM sightings;
