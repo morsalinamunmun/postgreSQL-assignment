190ee1fb9ddcf956fe7ab093aa5341cdada3f920
morsalinamunmun <morsalinamunmun@gmail.com>
2025-05-26 15:36:15 +0600

    label each sighting's time of day

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -88,3 +88,11 @@ SET conservation_status = 'Historic'
 WHERE discovery_date < '1800-01-01';
 
 
+-- Problem 8: Label each sighting's time of day
+SELECT sighting_id,
+    CASE
+        WHEN EXTRACT(HOUR FROM sighting_time) < 12 THEN 'Morning'
+        WHEN EXTRACT(HOUR FROM sighting_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS time_of_day
+FROM sightings;
