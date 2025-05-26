6b2661cdc93d2abaa1e2d6b1852b5053c01c277e
morsalinamunmun <morsalinamunmun@gmail.com>
2025-05-26 11:52:29 +0600

    Add species table creation and sample data insertion

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -14,3 +14,19 @@ INSERT INTO rangers (ranger_id, name, region) VALUES
 SELECT * FROM rangers;
 
 -- Problem-2: species table
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) NOT NULL
+);
+
+SELECT * FROM species;
+
+-- Insert sample data into species
+INSERT INTO species (species_id, common_name, scientific_name, discovery_date, conservation_status) VALUES
+(1, 'Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+(2, 'Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+(3, 'Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+(4, 'Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
