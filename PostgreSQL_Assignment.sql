75d4be1a27b256fcd024d43d19eed4948ad384da
morsalinamunmun <morsalinamunmun@gmail.com>
2025-05-26 14:41:31 +0600

    Add query to show the most recent 2 sightings with species and ranger details

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -74,4 +74,11 @@ WHERE species_id NOT IN (
     SELECT DISTINCT species_id FROM sightings
 );
 
+-- Problem 6: Show the most recent 2 sightings
+SELECT sp.common_name, s.sighting_time, r.name FROM sightings s
+JOIN species sp ON s.species_id = sp.species_id
+JOIN rangers r ON s.ranger_id = r.ranger_id
+ORDER BY s.sighting_time DESC
+LIMIT 2;
+
 
